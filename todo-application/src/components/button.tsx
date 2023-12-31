81d7d7e6a045e6e16d854e8443ed0a0a491a9b86
Adam Gorring <adamgorring@gmail.com>
2024-01-01 09:29:48 +1000

    Added colours and theme for frontend

---
+++ todo-application/src/components/button.tsx
@@ -0,0 +1,12 @@
+import { Box, Text } from "@/utils/themes";
+import { StyleSheet } from "react-native";
+
+const Button = () => {
+  return (
+    <Box bg="primary" p="4" borderRadius="rounded-3xl">
+      <Text color="white">Button</Text>
+    </Box>
+  );
+};
+
+export default Button;
---
+++ todo-application/src/utils/themes/colors.ts
@@ -0,0 +1,23 @@
+export const palette = {
+    white : "#ffffff",
+    primary: "#007AFF",
+    grey550: "#939292",
+    grey650: "#8a8a8a",
+    grey4: "#898681",
+    grey9: "#101928",
+    transparent: "transparent",
+    grey: "#878787",
+    blue200: "#90caf9"
+}
+
+export const colors = {
+    white : palette.white,
+    primary: palette.primary,
+    grey550: palette.grey550,
+    grey650: palette.grey650,
+    grey4: palette.grey4,
+    grey9: palette.grey9,
+    transparent: palette.transparent,
+    grey: palette.grey,
+    blue200: palette.blue200
+}
